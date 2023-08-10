3b1141a1bbb2c0124fbdba76087b19b8308f93d9
Mehmet Can Şakiroğlu <cansakiroglu@gmail.com>
2023-08-10 14:10:04 +0300

    Finish Scene and restarting is added

--- Assets/Scripts/CursorFreeAgain.cs
+++ Assets/Scripts/CursorFreeAgain.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CursorFreeAgain : MonoBehaviour
 {
+    
+    public BonnetController bonnetController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,16 @@ public class CursorFreeAgain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // When the scene is done:
+        if (bonnetController.done)
+        {
+            bonnetController.done = false;
+            Invoke("goToFinishScene", 3f);
+        }
+    }
+
+    void goToFinishScene()
+    {
+        SceneManager.LoadScene(sceneName: "FinishScene");
     }
 }
